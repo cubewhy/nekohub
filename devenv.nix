0814ddb93d5e071bb1b25958721315645bc5f330
cubewhy <qby140326@gmail.com>
2026-02-08 22:54:58 +0800

    chore: auto run `cargo sqlx prepare` with git pre-commit hook

--- devenv.nix
+++ devenv.nix
@@ -11,6 +11,8 @@
     openssl
     pkg-config
     sqlx-cli
+    bash
+    git
   ];
 
   # https://devenv.sh/languages/
@@ -22,6 +24,15 @@
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+  };
+
+  git-hooks.hooks.sqlx-prepare = {
+    enable = true;
+    name = "update sqlx offline cache";
+    entry = "bash -c 'cargo sqlx prepare -- --all-targets && git add .sqlx'";
+    files = "\\.(rs|sql)$";
+    language = "system";
+    pass_filenames = false;
   };
 
   # See full reference at https://devenv.sh/reference/options/
